6e3e5a4523e5caa66b3f67b6cab6a0eef0edfd7d
Simon Aittamaa <simon.aittamaa@neava.se>
2013-09-25 13:50:43 +0200

    Update filter script.
    Removed unused parameters and updates comments.

--- scripts/indexbraille.sh
+++ scripts/indexbraille.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-# Arguments, only the filename is used...
-job="$1"
-user="$2"
-title="$3"
-numcopies="$4"
-options="$5"
+# Arguments, only the filename ($6) is used atm.
 filename="$6"
 
-# Pass through, we only want to terminate the document with
-# ASCII SUB (see below) in order to start printing immediately.
+# Pass the document through unmolested. We only want to terminate
+# the document with ASCII SUB (see below) in order to start printing
+# immediately (printer should print after 5 seconds anyway, but
+# let's do things "right").
 cat "$filename"
 
 # ASCII SUB terminates document (032).
