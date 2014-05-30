0d5b87dbab9f7a21ba95301b449fa05880a53d33
Simon Aittamaa <simon.aittamaa@neava.se>
2014-05-30 09:44:07 +0200

    Added a GPLv3 header to C-source.

--- filter/index-direct-braille.c
+++ filter/index-direct-braille.c
@@ -1,12 +1,37 @@
+/**
+ * CUPS filter for printers supporting Index-direct-Braille.
+ * Copyright (C) 2014 Index Braille AB.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <errno.h>
 #include <signal.h>
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * We only buffer reading, output (to stdout) may or may not be buffered.
+ */
 static char buf[8096];
 static bytes_written;
 
+/**
+ * Send a single character to stdout, also keep track of number of bytes sent.
+ * @param c Character value to send.
+ */
 static void output(int c) {
 	/**
 	 * Keep track of number of bytes written, we wish to avoid a annoying
@@ -27,6 +52,18 @@ enum {
 	ESC=0x1b
 } escape_characters_t;
 
+/**
+ * Entry point for filter, parameters for filter in argv. Only copies (#4) and
+ * input file (#6) are used. It should be noted that (#6) is required atm.
+ *
+ * Signal SIGPIPE is ignored as suggested by the CUPS-filter programming
+ * manual. It should be noted that SIGTERM is _also_ ignored, since we
+ * currently have no way to abort the printout w/o corrupting the document.
+ *
+ * @param  argc Number of arguments.
+ * @param  argv Array or arguments.
+ * @return      0 upon success, 1 upon failure (see CUPS filter programming).
+ */
 int main(int argc, char **argv) {
 	char *tmp;
 	FILE* input;
